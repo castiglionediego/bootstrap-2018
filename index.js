d262df6c5d897f685cd3bd1a9b305d641b60e27a
Diego <diegojavier0610@gmail.com>
2019-07-16 08:33:16 -0300

    se agrego tooltip

---
+++ index.js
@@ -0,0 +1,6 @@
+//Indicamos que la función inicie solo si la página carga completamente.
+$(function(){
+	//$('[data-toggle="tooltip"]') seleccionamos el elemento de la página que tenga ese atributo
+	//Luego habilitamos el Tooltip con .tooltip()
+	$('[data-toggle="tooltip"]').tooltip()
+})
